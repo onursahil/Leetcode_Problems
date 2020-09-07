41ddd6a78ac5918eaa26dc382878ece10f17b717
onursahil <onursahilme@gmail.com>
2020-09-08 00:14:07 +0300

    leetcode XOR Operation In an Array Solution

---
+++ xor_operation_in_an_array.py
@@ -0,0 +1,46 @@
+"""
+
+Given an integer n and an integer start.
+
+Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+Return the bitwise XOR of all elements of nums.
+
+ 
+
+Example 1:
+
+Input: n = 5, start = 0
+Output: 8
+Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+Where "^" corresponds to bitwise XOR operator.
+
+
+Constraints:
+
+- 1 <= n <= 1000
+- 0 <= start <= 1000
+- n == nums.length
+
+"""
+
+from functools import reduce
+
+def xorOperation(n, start):
+    if n < 1 or n > 1000 or start < 1 or start > 1000:
+        return False
+    else:
+        nums = []
+        for i in range(n):
+            nums.append(start + (2 * i))
+        
+        if n != len(nums):
+            return False
+            
+        answer = reduce(lambda i, j: int(i) ^ int(j), nums)
+    
+    return answer
+
+n, start = 10, 5
+result = xorOperation(n, start)
+print(result)
