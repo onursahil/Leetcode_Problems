d319f1119b81539f34ba5285e7600a44b0d60466
onursahil <onursahilme@gmail.com>
2021-02-21 01:20:16 +0300

    leetcode Longest Nice Substring Solution

---
+++ longest_nice_substring.py
@@ -0,0 +1,22 @@
+def longestNiceSubstring(s):
+    N = len(s)
+    best = ""
+
+    def good(start, end):
+        t = set(s[start:end])
+
+        for x in t:
+            if (x.lower() in t) != (x.upper() in t):
+                return False
+        return True
+    
+    for start in range(N):
+        for end in range(start, N + 1):
+            if good(start, end) and end - start + 1 > len(best):
+                best = s[start:end]
+
+    return best
+
+s = "BebjJE"
+result = longestNiceSubstring(s)
+print(result)
